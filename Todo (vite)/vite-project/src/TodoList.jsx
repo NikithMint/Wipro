0a4a412fa05da45282ca0199051f701025ba0e36
Nikith Tankashala <108892614+NikithMint@users.noreply.github.com>
2025-03-04 14:20:23 +0530

    Todolist

---
+++ Todo (vite)/vite-project/src/TodoList.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+function TodoList(){
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+        setNewTask(event.target.value);
+    }
+
+    function addTask(){
+    
+        setTasks([...tasks,newTask])
+        setNewTask("")
+    }
+
+    function deleteTask(index){
+        tasks.splice(index,1)
+        setTasks([...tasks])
+    }
+   
+
+    return(
+    <div>
+
+        <h1>To-Do-List</h1>
+
+        <div>
+            <input
+                type="text"
+                placeholder="Enter a task..."
+                value={newTask}
+                onChange={handleInputChange}/>
+            <button
+                
+                onClick={addTask}>
+                Add
+            </button>
+        </div>
+        <ol>
+            {tasks.map((task, index) => 
+                <li key={index}>
+                    <span >{task}</span>
+                    <button
+                        
+                        onClick={() => deleteTask(index)}>
+                        Delete
+                    </button>
+                    <button onClick={()=>edit(index)}>edit</button>
+                </li>
+            )}
+        </ol>
+
+        <h1>{newTask}</h1>
+        <h1>{editt}</h1>
+    </div>);
+}
+export default TodoList
