ef1ff4f4d6458e24a1ce499644d022633226af8c
Nikith Tankashala <108892614+NikithMint@users.noreply.github.com>
2025-03-09 10:29:59 +0530

    New

---
+++ UI (vite)/src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+    <div class="nav-left">BESNARD</div>
+        <div class="nav-right">
+            <a href="#">Login</a>
+            <a href="#">Search</a>
+            <a href="#">Cart</a>
+        </div>
+    </nav>
+
+  )
+}
+
+export default Navbar
